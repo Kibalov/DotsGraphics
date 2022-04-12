9ff41d12fc7cb3c352ce390efd637ec63d48dc89
Kibalov <kvv.20@uni-dubna.ru>
2022-04-13 00:28:51 +0300

    first

---
+++ PPRzad8/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace PPRzad8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Нарисовать = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.DashDot;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Series0";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bubble;
+            series3.Legend = "Legend1";
+            series3.MarkerSize = 3;
+            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series3.Name = "Series2";
+            series3.YValuesPerPoint = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bubble;
+            series4.Legend = "Legend1";
+            series4.MarkerSize = 3;
+            series4.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series4.Name = "Series3";
+            series4.YValuesPerPoint = 2;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bubble;
+            series5.Legend = "Legend1";
+            series5.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Star10;
+            series5.Name = "Series5";
+            series5.YValuesPerPoint = 2;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bubble;
+            series6.Legend = "Legend1";
+            series6.MarkerColor = System.Drawing.Color.DarkMagenta;
+            series6.MarkerSize = 7;
+            series6.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Diamond;
+            series6.Name = "Series6";
+            series6.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(788, 308);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Paint += new System.Windows.Forms.PaintEventHandler(this.chart1_Paint);
+            this.chart1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseDown);
+            // 
+            // Нарисовать
+            // 
+            this.Нарисовать.Location = new System.Drawing.Point(12, 326);
+            this.Нарисовать.Name = "Нарисовать";
+            this.Нарисовать.Size = new System.Drawing.Size(121, 46);
+            this.Нарисовать.TabIndex = 1;
+            this.Нарисовать.Text = "Нарисовать";
+            this.Нарисовать.UseVisualStyleBackColor = true;
+            this.Нарисовать.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 326);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 352);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(90, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Проверить точку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 352);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 410);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Нарисовать);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button Нарисовать;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PPRzad8/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PPRzad8
+{
+    public partial class Form1 : Form
+    {
+        double[,,] dots;
+        public Form1()
+        {
+            InitializeComponent();
+            ReadFromTXTData();
+        }
+
+        private void ReadFromTXTData()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            DialogResult = openFileDialog.ShowDialog();
+            if (DialogResult == DialogResult.OK)
+            {
+                dots = new double[2, 2, 100];
+                StreamReader streamReader = new StreamReader(openFileDialog.FileName);
+                for (int i = 0; i < 100; i++)
+                {
+                    string[] nums = streamReader.ReadLine().Split('\t');
+                    dots[0, 0, i] = Convert.ToDouble(nums[0]);
+                    dots[0, 1, i] = Convert.ToDouble(nums[1]);
+                    dots[1, 0, i] = Convert.ToDouble(nums[2]);
+                    dots[1, 1, i] = Convert.ToDouble(nums[3]);
+                }
+                streamReader.Close();
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //if (dots != null)
+            //{
+            //    Brush brushOrange = new SolidBrush(Color.Orange);
+            //    Brush brushBlue = new SolidBrush(Color.Blue);
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        e.Graphics.FillEllipse(brushBlue, (int)dots[0, 0, i] * Width/45, (int)dots[0, 1, i] * Height / 60, 5, 5);
+            //        e.Graphics.FillEllipse(brushOrange, (int)dots[1, 0, i] * Width / 45, (int)dots[1, 1, i] * Height / 60, 5, 5);
+            //    }
+            //}
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                chart1.Series[0].Points.AddXY(dots[0, 0, i], dots[0, 1, i]);
+                chart1.Series[1].Points.AddXY(dots[1, 0, i], dots[1, 1, i]);
+
+            }
+            Graphics graphics = chart1.CreateGraphics();
+            MessageBox.Show(chart1.Series[0].Points.FindMaxByValue("X", 0).XValue.ToString() + " " +
+                chart1.Series[0].Points.FindMinByValue("X", 0).XValue.ToString() + " " +
+                chart1.Series[0].Points.FindMaxByValue("Y", 0).YValues[0].ToString() + " " +
+                chart1.Series[0].Points.FindMinByValue("Y", 0).YValues[0].ToString() + " \n" +
+                chart1.Series[1].Points.FindMaxByValue("X", 0).XValue.ToString() + " " +
+                chart1.Series[1].Points.FindMinByValue("X", 0).XValue.ToString() + " " +
+                chart1.Series[1].Points.FindMaxByValue("Y", 0).YValues[0].ToString() + " " +
+                chart1.Series[1].Points.FindMinByValue("Y", 0).YValues[0].ToString());
+
+            chart1.Series[2].Points.AddXY(chart1.Series[1].Points.FindMaxByValue("X", 0).XValue,
+                chart1.Series[1].Points.FindMaxByValue("X", 0).YValues[0]);
+            chart1.Series[2].Points.AddXY(chart1.Series[1].Points.FindMinByValue("Y", 0).XValue,
+                chart1.Series[1].Points.FindMinByValue("Y", 0).YValues[0]);
+            chart1.Series[2].Points.AddXY(chart1.Series[1].Points.FindMinByValue("X", 0).XValue,
+                chart1.Series[1].Points.FindMinByValue("X", 0).YValues[0]);
+            chart1.Series[2].Points.AddXY(chart1.Series[1].Points.FindMaxByValue("Y", 0).XValue,
+                chart1.Series[1].Points.FindMaxByValue("Y", 0).YValues[0]);
+
+            chart1.Series[3].Points.AddXY(chart1.Series[0].Points.FindMaxByValue("X", 0).XValue,
+                chart1.Series[0].Points.FindMaxByValue("X", 0).YValues[0]);
+            chart1.Series[3].Points.AddXY(chart1.Series[0].Points.FindMinByValue("Y", 0).XValue,
+                chart1.Series[0].Points.FindMinByValue("Y", 0).YValues[0]);
+            chart1.Series[3].Points.AddXY(chart1.Series[0].Points.FindMinByValue("X", 0).XValue,
+                chart1.Series[0].Points.FindMinByValue("X", 0).YValues[0]);
+            chart1.Series[3].Points.AddXY(chart1.Series[0].Points.FindMaxByValue("Y", 0).XValue,
+                chart1.Series[0].Points.FindMaxByValue("Y", 0).YValues[0]);
+        }
+
+        private void chart1_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.DrawRectangle(new Pen(Color.Blue, 2), 475, 49, 175, 63);
+            //e.Graphics.DrawEllipse(new Pen(Color.Orange, 2), 73, 74, 410, 183);
+        }
+
+        private void chart1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(e.X + "  " + e.Y);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            //chart1.Series[1].Points.FindMaxByValue("X", 0).XValue.ToString() + " " +
+            //chart1.Series[1].Points.FindMinByValue("X", 0).XValue.ToString() + " " +
+            //chart1.Series[1].Points.FindMaxByValue("Y", 0).YValues[0].ToString() + " " +
+            //chart1.Series[1].Points.FindMinByValue("Y", 0).YValues[0].ToString());
+
+
+            bool obl1 = false;
+            double xmax = chart1.Series[0].Points.FindMaxByValue("X", 0).XValue;
+            double xmin = chart1.Series[0].Points.FindMinByValue("X", 0).XValue;
+            double ymax = chart1.Series[0].Points.FindMaxByValue("Y", 0).YValues[0];
+            double ymin = chart1.Series[0].Points.FindMinByValue("Y", 0).YValues[0];
+            if (x > xmin && x < xmax && y < ymax && y > ymin)
+            {
+                obl1 = true;
+            }
+
+
+            chart1.Series[4].Points.AddXY(x, y);
+            double a = Math.Abs(chart1.Series[1].Points.FindMaxByValue("X", 0).XValue - chart1.Series[1].Points.FindMinByValue("X", 0).XValue);
+            double b = Math.Abs(chart1.Series[1].Points.FindMaxByValue("Y", 0).YValues[0] - chart1.Series[1].Points.FindMinByValue("Y", 0).YValues[0]);
+            double x0 = (chart1.Series[1].Points.FindMaxByValue("X", 0).XValue + chart1.Series[1].Points.FindMinByValue("X", 0).XValue) / 2;
+            double y0 = (chart1.Series[1].Points.FindMaxByValue("Y", 0).YValues[0] + chart1.Series[1].Points.FindMinByValue("Y", 0).YValues[0]) / 2;
+
+            chart1.Series[5].Points.AddXY(x0, y0);
+            x -= x0;
+            y -= y0;
+            //MessageBox.Show(x+" "+y+" "+a+" "+b+" "+x0+" "+y0);
+
+            bool obl2 = false;
+            if ((Math.Pow(x,2)/Math.Pow(a/2,2) + Math.Pow(y, 2) / Math.Pow(b/2, 2)) <= 1)
+                obl2 = true;
+
+            if (obl1 && obl2)
+                MessageBox.Show("Точка попадает в обе области");
+            else if (obl1)
+                MessageBox.Show("Точка попадает в первую область");
+            else if (obl2)
+                MessageBox.Show("Точка попадает во вторую область");
+            else
+                MessageBox.Show("Точка не попадает ни в какую область");
+        }
+    }
+}
